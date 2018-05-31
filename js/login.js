c7400f687aab98e5247551908a63d5f5892720e7
Claire joo <claire.m.joo@gmail.com>
2018-05-31 20:58:58 +1000

    Call login API and save user token in session storage

---
+++ js/login.js
@@ -0,0 +1,24 @@
+$('#loginForm').submit(function(event){
+    event.preventDefault();
+    
+    var loginData = $('#loginForm').serialize();
+    
+    $.ajax({
+        url: 'http://54.79.111.71:1337/api/user/login',
+        headers: {
+            'Content-Type':'application/x-www-form-urlencoded'
+        },
+        method: 'POST',
+        data: loginData,
+        success: function(res){
+            // store user token
+            window.sessionStorage.setItem('userToken', res.data.token);
+            
+            // go to main page
+        },
+        error: function(){
+            console.log('error');
+        }
+    });
+});
+
